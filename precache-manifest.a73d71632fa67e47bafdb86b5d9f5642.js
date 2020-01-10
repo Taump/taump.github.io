ee59cd06245449907646653c88eed65a53693907
Taump <TAUMP@YANDEX.RU>
2020-01-10 19:43:29 +0700

    update panel

--- precache-manifest.a73d71632fa67e47bafdb86b5d9f5642.js
+++ precache-manifest.a73d71632fa67e47bafdb86b5d9f5642.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2f12ea120ac191ace52385c31659e9bc",
-    "url": "/index.html"
+    revision: "2f12ea120ac191ace52385c31659e9bc",
+    url: "/index.html"
   },
   {
-    "revision": "b479eb9ddbf775a9399e",
-    "url": "/static/css/2.c7b90ac4.chunk.css"
+    revision: "b479eb9ddbf775a9399e",
+    url: "/static/css/2.c7b90ac4.chunk.css"
   },
   {
-    "revision": "ae4140dba366b8afa4f0",
-    "url": "/static/css/main.6446de45.chunk.css"
+    revision: "ae4140dba366b8afa4f0",
+    url: "/static/css/main.6446de45.chunk.css"
   },
   {
-    "revision": "b479eb9ddbf775a9399e",
-    "url": "/static/js/2.a1c4871e.chunk.js"
+    revision: "b479eb9ddbf775a9399e",
+    url: "/static/js/2.a1c4871e.chunk.js"
   },
   {
-    "revision": "8399f1f42243f6955183caa60962ef47",
-    "url": "/static/js/2.a1c4871e.chunk.js.LICENSE"
+    revision: "8399f1f42243f6955183caa60962ef47",
+    url: "/static/js/2.a1c4871e.chunk.js.LICENSE"
   },
   {
-    "revision": "ae4140dba366b8afa4f0",
-    "url": "/static/js/main.00dff651.chunk.js"
+    revision: "ae4140dba366b8afa4f0",
+    url: "/static/js/main.00dff651.chunk.js"
   },
   {
-    "revision": "692753ad3013d73afbc519fa46bd4e27",
-    "url": "/static/js/main.00dff651.chunk.js.LICENSE"
+    revision: "692753ad3013d73afbc519fa46bd4e27",
+    url: "/static/js/main.00dff651.chunk.js.LICENSE"
   },
   {
-    "revision": "69c650e99df4eb028a3d",
-    "url": "/static/js/runtime-main.f1d6c04d.js"
+    revision: "69c650e99df4eb028a3d",
+    url: "/static/js/runtime-main.f1d6c04d.js"
   }
 ]);

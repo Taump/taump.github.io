023aa713dee189508aabcf9b53eddfca6c2c86de
Taump <TAUMP@YANDEX.RU>
2020-01-10 19:52:23 +0700

    update

---
+++ precache-manifest.88816ab7220101e8a933ff1efab555ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc08fe818afd45bc9d61d3aaa38e0dcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "249749647e1702ba9002",
+    "url": "/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "adc5c1ca15bcd5773f14",
+    "url": "/static/css/main.72f46cd4.chunk.css"
+  },
+  {
+    "revision": "249749647e1702ba9002",
+    "url": "/static/js/2.904eaa05.chunk.js"
+  },
+  {
+    "revision": "e220114b51e69fbcc6a278ed7c586b32",
+    "url": "/static/js/2.904eaa05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "adc5c1ca15bcd5773f14",
+    "url": "/static/js/main.fc9a043f.chunk.js"
+  },
+  {
+    "revision": "69c650e99df4eb028a3d",
+    "url": "/static/js/runtime-main.f1d6c04d.js"
+  }
+]);
